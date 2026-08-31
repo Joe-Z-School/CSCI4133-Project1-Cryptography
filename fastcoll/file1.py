7c9eb2d2be4c4c3ad1ab38069612f7fc0bf3d4ec
Joe Zakrzewski <joe.zakrzewski@colorado.edu>
2026-08-31 16:56:45 -0600

    Adding files without null characters

---
+++ fastcoll/file1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# coding: latin-1
+blob = """AAAAAAAAAAAAAAAAA"R÷C9¨15ºa-0JwàˆHeu&–Aùï¿Î$Álô‘KÆZÄ•:dK(t÷½ˆ<­‘xëê´Ä^™Tİ&4ò`}P„ç(ãñoê;ı9„Ÿ{Ë_'AØò´ª…è£^ª™ëd²şğä”ÔÑ±ÿ–FÆá¶Bt½>"""
+from hashlib import sha256
+print(sha256(blob.encode("latin-1")).hexdigest())
---
+++ fastcoll/file2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# coding: latin-1
+blob = """AAAAAAAAAAAAAAAAA"R÷C9¨15ºa-0JwàˆÈeu&–Aùï¿Î$Álô‘KÆZÄ;dK(t÷½ˆ<­‘øëê´Ä^™Tİ&4ò`}P„ç(ãñïê;ı9„Ÿ{Ë_'AØò´ª…è£^ª™ëd2şğä”ÔÑ±ÿ–FÆá6Bt½>"""
+from hashlib import sha256
+print(sha256(blob.encode("latin-1")).hexdigest())
